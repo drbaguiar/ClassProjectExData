c750186cbdc6d3c944dbe2c317069d6e96b687f8
Dr.B <drbaguiar@gmail.com>
2014-10-10 13:42:00 -0500

    Updated
    Updated R files

--- plot2.R
+++ plot2.R
@@ -5,22 +5,26 @@ if (!file.exists("dataset.zip")) {
         download.file(zip.url, destfile = zip.file)
         unzip(zip.file)
 }
-# Read text file
-datafull <-read.table("household_power_consumption.txt", header = TRUE, sep =";")
 
-#Convert Global_active_power to numeric and date to date
-datafull$Global_active_power <- as.numeric(datafull$Global_active_power)
-datafull$Date = as.Date(datafull$Date, format = "%d/%m/%Y" )
+if (!exists("data")){
+        
+        # Read text file
+        datafull <-read.table("household_power_consumption.txt", header = TRUE, sep =";")
 
-#Get the relevant records
-data <- datafull[(datafull$Date == "2007-02-01" | datafull$Date == "2007-02-02") , ]
+        #Convert Global_active_power to numeric and date to date
+        datafull$Global_active_power <- as.numeric(datafull$Global_active_power)
+        datafull$Date = as.Date(datafull$Date, format = "%d/%m/%Y" )
 
-#Remove the full file 
-rm(datafull)
+        #Get the relevant records
+        data <- datafull[(datafull$Date == "2007-02-01" | datafull$Date == "2007-02-02") , ]
 
-#Convert date and time
-datetime <- paste(data$Date, data$Time)
-data$Datetime <- as.POSIXct(datetime)
+        #Convert date and time
+        datetime <- paste(data$Date, data$Time)
+        data$Datetime <- as.POSIXct(datetime)
+
+        #Remove the full file 
+        rm(datafull)
+}
 
 ## write output to .png file
 png(filename = "plot2.png", width = 480, height = 480, units = "px", pointsize = 12, bg = "white")
