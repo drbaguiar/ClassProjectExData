0fb55537cea46a0a2038b806fddf405926ff4a8c
Dr.B <drbaguiar@gmail.com>
2014-11-01 22:50:14 -0500

    Updated Directory

--- plot2.R
+++ plot2.R
@@ -1,7 +1,7 @@
 #Check for the File. If not there, download the data and extract it
-if (!file.exists("dataset.zip")) {
+if (!file.exists("C:/Users/bryan_000/Documents/GitHub/Data/power_dataset.zip")) {
         zip.url <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
-        zip.file <- 'dataset.zip'
+        zip.file <- 'C:/Users/bryan_000/Documents/GitHub/Data/power_dataset.zip'
         download.file(zip.url, destfile = zip.file)
         unzip(zip.file)
 }
@@ -9,7 +9,7 @@ if (!file.exists("dataset.zip")) {
 #Check to see if the data has been processed
 if (!exists("datasub")){
         # Read text file
-        datafull <-read.table("household_power_consumption.txt", header = TRUE, sep =";")
+        datafull <-read.table("C:/Users/bryan_000/Documents/GitHub/Data/household_power_consumption.txt", header = TRUE, sep =";")
         
         #clean the file
         names(datafull) <-tolower(names(datafull))
@@ -31,7 +31,7 @@ if (!exists("datasub")){
 }
 
 ## write output to .png file
-png(filename = "plot2.png", width = 480, height = 480, units = "px", pointsize = 12, bg = "white")
+png(filename = "C:/Users/bryan_000/Documents/GitHub/Data/plot2.png", width = 480, height = 480, units = "px", pointsize = 12, bg = "white")
 
 ## write output to .png file
 with (datasub, plot(datasub$globalactivepower~datasub$datetime, type="l", ylab="Global Active Power (kilowatts)", xlab=""))
