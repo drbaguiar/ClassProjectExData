88abe3e22596d2e1e16e697d00a43755e34a3cd0
Dr.B <drbaguiar@gmail.com>
2014-11-09 19:16:47 -0600

    Changed Directory

--- plot4.R
+++ plot4.R
@@ -1,15 +1,18 @@
+##Set working directory to data directory
+setwd('C:/Users/bryan_000/Documents/GitHub/Data/')
+
 #Check for the File. If not there, download the data and extract it
-if (!file.exists("C:/Users/bryan_000/Documents/GitHub/Data/power_dataset.zip")) {
+if (!file.exists("power_dataset.zip")) {
         zip.url <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
-        zip.file <- 'C:/Users/bryan_000/Documents/GitHub/Data/power_dataset.zip'
-        download.file(zip.url, destfile = zip.file)
+        zip.file <- 'power_dataset.zip'
+        download.file(zip.url, zip.file)
         unzip(zip.file)
 }
 
 #Check to see if the data has been processed
 if (!exists("datasub")){
         # Read text file
-        datafull <-read.table("C:/Users/bryan_000/Documents/GitHub/Data/household_power_consumption.txt", header = TRUE, sep =";")
+        datafull <-read.table("household_power_consumption.txt", header = TRUE, sep =";")
         
         #clean the file
         names(datafull) <-tolower(names(datafull))
@@ -31,7 +34,7 @@ if (!exists("datasub")){
 }
 
 ## write output to .png file
-png(filename = "C:/Users/bryan_000/Documents/GitHub/Data/plot4.png", width = 480, height = 480, units = "px", pointsize = 12, bg = "white")
+png(filename = "plot4.png", width = 480, height = 480, units = "px", pointsize = 12, bg = "white")
 
 ## write output to .png file
 par(mfrow = c(2, 2))
